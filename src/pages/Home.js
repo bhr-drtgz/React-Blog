347723beb8a08dff341ea52d702314f90cbd072e
bhr-drtgz <akrepwkral@gmail.com>
2023-03-11 18:44:10 +0300

    Login Sayfası Düzenlendi

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
 
 const Home = () => {
+  const { loginState } = useSelector(state => state)
+  console.log(loginState)
   return (
-    <div>ANA SAYFA</div>
+
+    <div>
+      {
+        loginState.success === false && (
+          <Link to={"/login"}>Login</Link>
+        )
+      }
+      <h1>ANASAYFA</h1>
+    </div>
   )
 }
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Form, Button } from 'react-bootstrap'
 
 const Login = () => {
+
+    const [form, setForm] = useState({
+        username: "",
+        password: ""
+    })
+
+    const handleLogin = (event) => {
+        event.preventDefault()
+        console.log(form)
+    }
+
     return (
-        <div>Login</div>
+        <div className='container my-5'>
+            <h1 className='text-center'>Login</h1>
+            <Form onSubmit={handleLogin}>
+                <Form.Group className="mb-3">
+                    <Form.Label>UserName</Form.Label>
+                    <Form.Control value={form.username} onChange={(event) => setForm({ ...form, username: event.target.value })} type="text" placeholder="Enter UserName" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control value={form.password} onChange={(event) => setForm({ ...form, password: event.target.value })} type="password" placeholder="Password" />
+                </Form.Group>
+                <div className='my-5 d-flex justify-content-center'>
+                    <Button className='w-50' variant="primary" type="submit">
+                        Login
+                    </Button>
+                </div>
+            </Form>
+        </div >
     )
 }
 
