783a21e809a76f741a33bc7d2e3805393557870d
bhr-drtgz <akrepwkral@gmail.com>
2023-03-12 20:18:53 +0300

    Login tamamlandı

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,16 +1,41 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Form, Button } from 'react-bootstrap'
+import { useSelector, useDispatch } from 'react-redux'
+import actionTypes from './../redux/actions/actionTypes';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+    const { usersState, loginState } = useSelector(state => state)
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
 
     const [form, setForm] = useState({
         username: "",
         password: ""
     })
 
+    useEffect(() => {
+        if (loginState.success)
+            navigate("/")
+    }, [])
+
     const handleLogin = (event) => {
         event.preventDefault()
         console.log(form)
+        console.log(usersState)
+        const hasUsers = usersState.users.find(item => item.username === form.username)
+        console.log(hasUsers)
+        if (hasUsers === undefined) {
+            alert("Böyle Bir Kullanıcı Bulunamadı!!")
+            return
+        }
+        if (hasUsers.password !== form.password) {
+            alert("Şifreniz Hatalı !!!")
+            return
+
+        }
+        dispatch({ type: actionTypes.loginActions.LOGIN_SUCCESS, payload: { username: hasUsers.username, role: hasUsers.role }, })
+        navigate("/")
     }
 
     return (
